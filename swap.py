357fe42b0071a0ab3e983de24fd3ef050a6c18e1
Harajuku Girl <n.al-wattar@kew.org>
2019-02-11 17:42:25 +0000

    fastplaststyleid

--- swap.py
+++ swap.py
@@ -2,26 +2,27 @@ import sys
 import Bio.SeqIO
 import Bio.SeqRecord
 import re
+import fastplaststuff
 
 def readSeqRecordList(fastaFname):
     allRecordsList = []
     for seqRecord in Bio.SeqIO.parse(fastaFname, 'fasta'):
         allRecordsList.append(seqRecord)
-    #print(allRecordsList)
     return allRecordsList
+
+def acquireFastPlastStyleID(seqRecordList):
+    for seqRecord in seqRecordList:
+        fastplastStyleID = seqRecord.id
+        print(fastplaststuff.geneAndOrganismSeparator(fastplastStyleID))
+        print(fastplastStyleID)
 
 def formatIDsToTargetsFile(seqRecordList):
     for seqRecord in seqRecordList:
-        geneAndOrganismList = seqRecord.id.split('[_]') #IN PLACE PROCESSING!
-        print(len(geneAndOrganismList))
         if len(geneAndOrganismList) != 2:
             raise Exception('length is greater than 2: %s' % seqRecord.id)
             #unusualNames = seqRecord.id +'\n'
             #sys.stderr.write(unusualNames)
-        geneName = geneAndOrganismList[0]
-        organismName = geneAndOrganismList[1]
-        formattedID = '%s-%s' % (organismName, geneName)
-        #print(formattedID)
+        formattedID = '%s-%s' % (organism, gene)
         seqRecord.id = formattedID
 
 def writeNamesWithMoreThanOneUnderscoreToStderr(seqRecordList):
@@ -30,11 +31,11 @@ def writeNamesWithMoreThanOneUnderscoreToStderr(seqRecordList):
         if len(geneAndOrganismList) != 2:
             unusualNames = seqRecord.id +'\n'
             sys.stderr.write(unusualNames)
-            #print(unusualNames)
 
 fastaFname = sys.argv[1]
 seqRecordList = readSeqRecordList(fastaFname)
+#print(seqRecordList)
+acquireFastPlastStyleID(seqRecordList)
 #formatIDsToTargetsFile(seqRecordList)
-writeNamesWithMoreThanOneUnderscoreToStderr(seqRecordList)
 #targetsFastaFname = sys.argv[2]
 #Bio.SeqIO.write(seqRecordList, targetsFastaFname, 'fasta')
